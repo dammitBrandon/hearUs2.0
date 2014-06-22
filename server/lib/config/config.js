e37bd18fda295decb427b28d4c88720a875f829b
btbailey <brandon.bailey@slalom.com>
2014-06-22 17:32:04 -0500

    sunlight labs api key added

--- server/lib/config/config.js
+++ server/lib/config/config.js
@@ -5,6 +5,11 @@ var _ = require('lodash');
 /**
  * Load environment configuration
  */
+
+var config = {
+    apikey: "d9836e25aa044e26bf6008212dab2514"
+};
+
 module.exports = _.extend(
     require('./env/all.js'),
     require('./env/' + process.env.NODE_ENV + '.js') || {});
