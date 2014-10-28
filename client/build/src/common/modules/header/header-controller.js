6672179f39d894060e65540a0fd9ed6fa81e3869
btbailey <brandon.bailey@slalom.com>
2014-10-28 13:23:32 -0500

    removed cliend/build, added WIP heroku instance data, need mongodb password etup in the productionjs file.

--- client/build/src/common/modules/header/header-controller.js
+++
@@ -1,8 +0,0 @@
-angular.module('header.controller', [])
-  .controller('HeaderCtrl', function ($scope) {
-    $scope.user = {
-      username: 'pmaccart',
-      firstName: 'Phil',
-      lastName: 'MacCart'
-    };
-  });
--- client/build/src/common/modules/header/header.js
+++
@@ -1,4 +0,0 @@
-angular.module('header', [
-    'ui.router',
-    'header.controller'
-  ]);
