6f2cf9f80315913937f477618d36b1da43d5c2de
btbailey <brandon.bailey@slalom.com>
2015-02-13 11:45:22 -0600

    added requesting additional information modal into the resolve, there are some bugs that need to be resolved, and issues will be opened to document these

--- client/src/app/districtSearch/districtSeach.js
+++ client/src/app/districtSearch/districtSeach.js
@@ -22,7 +22,7 @@ angular.module('districtSearch', [
           controller: "districtSearchCtrl",
           templateUrl: "districtSearch/district-landing-template.html",
           resolve: {
-            districtInfo: function ($log, $stateParams, SunlightService, ModalService) {
+            districtInfo: function ($log, $stateParams, SunlightService, $modal, ModalService) {
               return SunlightService.getDistrictByZipCode($stateParams.zipCode).then(function (districtData) {
                 var district = {};
                 if (districtData.count === 1 || districtData.count === 0) {
@@ -42,7 +42,50 @@ angular.module('districtSearch', [
 //                  4. we can get them to enter the +4 for the zip code
 
                   $log.log('pop up modal', districtData);
-                  ModalService.openModal();
+//                  ModalService.openModal($stateParams.zipCode);
+
+                  var modalDefaults = {
+                    template: "<div>" +
+                      "<h3>We Need more info!</h3>" +
+                      " <div>" +
+                      "   <p>We were unable to determine your district please provied additional information.</p>" +
+                      "   <input type='text' placeholder='Street Address' ng-model='streetAddress'/>" +
+                      "<div class='row collapse'>" +
+                      "   <div class='columns small-4 large-3'>" +
+                      "     <span class='prefix'>{{zipCode}}</span>" +
+                      "   </div>" +
+                      "   <div class='columns small-8 large-9'>" +
+                      "     <input type='text' placeholder='+ 4'/>" +
+                      "   </div>" +
+                      "</div>" +
+                      "<div class='row collapse'>" +
+                      "   <div class='columns small-4 large-3'>" +
+                      "     <span class='prefix'>Geolocate</span>" +
+                      "   </div>" +
+                      "   <div class='columns small-8 large-9'>" +
+                      "   <hu-geolocate ng-click='ok()'></hu-geolocate>" +
+                      "   </div>" +
+                      "</div>" +
+                      "   <button class='button left' ng-click='cancel()'>Cancel</button>" +
+                      "   <button class='button right' ng-click='ok()'>Find</button>" +
+                      " </div>" +
+                      "</div>",
+                    backdrop: true,
+                    keyboard: true,
+                    controller: function ($scope, $modalInstance) {
+                      $scope.zipCode = $stateParams.zipCode;
+                      $scope.ok = function () {
+                        $modalInstance.close();
+                      };
+
+                      $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                      };
+                    }
+                  };
+
+                  $modal.open(modalDefaults);
+
                   var districtsArray = _.map(districtData.results, function (districtNumber) {
                     return districtNumber.district;
 
