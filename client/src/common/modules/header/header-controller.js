f5139ae222fbebc0033690fd41c3253b527f232f
btbailey <brandon.bailey@slalom.com>
2015-02-19 23:54:54 -0600

    refactored header implementing foundation directive

--- client/src/common/modules/header/header-controller.js
+++ client/src/common/modules/header/header-controller.js
@@ -1,8 +1,5 @@
 angular.module('header.controller', [])
   .controller('HeaderCtrl', function ($scope) {
     $scope.user = {
-      username: 'pmaccart',
-      firstName: 'Phil',
-      lastName: 'MacCart'
     };
   });
