b82437d38d739932596e2faeddceb133cdd2a5b0
btbailey <brandon.bailey@slalom.com>
2015-02-23 14:58:10 -0600

    on state change to landing page modal is dismissed

--- client/src/app/districtSearch/districtSearch-controller.js
+++ client/src/app/districtSearch/districtSearch-controller.js
@@ -56,6 +56,12 @@ angular.module('districtSearch.controllers', [
           $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
           };
+
+          $rootScope.$on('$stateChangeStart', function(event, toState, fromState, fromParams) {
+            if (toState.url === "/") {
+              $modalInstance.dismiss('cancel');
+            }
+          });
         }
       };
 
