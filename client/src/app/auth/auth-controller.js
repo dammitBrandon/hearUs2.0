75cb477256fbe0abcc1d9c7258607744806ba8be
btbailey <brandon.bailey@slalom.com>
2015-02-26 17:50:20 -0600

    WIP, sign up form validation added

--- client/src/app/auth/auth-controller.js
+++ client/src/app/auth/auth-controller.js
@@ -34,5 +34,22 @@ angular.module('auth.controllers', [
       profileService.logOut();
     };
     
+    $scope.passwordEquality = function() {
+      if($scope.hearUsForm['sign-up'].userPassword.$touched && $scope.hearUsForm['sign-up'].userPassword.$valid) {
+        $log.log('password input valid and has been touched');
+        if(angular.equals($scope.user.password, $scope.verifyPassword)) {
+          $log.log('password and verify password same');
+        } else {
+          $log.log('password and verify password not the same');
+          $scope.hearUsForm['sign-up'].verifyPassword.$invalid = true;
+        }
+      } else {
+        $log.log('password field invalid or untouched');
+      }
+    };
+    
+    $scope.signUpFormValid = function() {
+    };
+    
     initAuthPage();
   });
