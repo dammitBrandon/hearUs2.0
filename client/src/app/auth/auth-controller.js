13b286a847da5134065353ab5e031788e66beb7c
btbailey <brandon.bailey@slalom.com>
2015-02-26 19:59:05 -0600

    validation added to sign in form
    Conflicts:
    	client/src/app/auth/sign-in.jade

--- client/src/app/auth/auth-controller.js
+++ client/src/app/auth/auth-controller.js
@@ -36,26 +36,18 @@ angular.module('auth.controllers', [
     
     $scope.passwordEquality = function() {
       if(($scope.hearUsForm['sign-up'].userPassword.$touched && $scope.hearUsForm['sign-up'].userPassword.$valid) || $scope.hearUsForm['sign-up'].verifyPassword.$touched) {
-        $log.log('password input valid and has been touched');
         if(angular.equals($scope.user.password, $scope.verifyPassword)) {
-          $log.log('password and verify password same');
           if ($scope.hearUsForm['sign-up'].verifyPassword.$invalid === true ) {
             $scope.hearUsForm['sign-up'].verifyPassword.$invalid = false;
           }
         } else {
-          $log.log('password and verify password not the same');
           $scope.hearUsForm['sign-up'].verifyPassword.$invalid = true;
         }
-      } else {
-        $log.log('password field invalid or untouched');
       }
     };
     
-    $scope.isFormValid= function() {
+    $scope.isSignUpFormValid= function() {
       return (!$scope.hearUsForm['sign-up'].$invalid && !$scope.hearUsForm['sign-up'].userPassword.$pristine && angular.equals($scope.user.password, $scope.verifyPassword));
-    };
-    
-    $scope.signUpFormValid = function() {
     };
     
     initAuthPage();
