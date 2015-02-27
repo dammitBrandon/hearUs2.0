981dcd476edea2638122e94ef7cd8fd1e55930a4
btbailey <brandon.bailey@slalom.com>
2015-02-27 09:29:57 -0600

    input validation check on landing page searches
    Conflicts:
    	client/src/app/landing/landing-template.jade

--- client/src/app/landing/landing-controller.js
+++ client/src/app/landing/landing-controller.js
@@ -12,4 +12,20 @@ angular.module('landing.controllers', [
     $scope.findDistrictLanding = function findDistrict() {
       $state.go('districtSearch.zipCode', {zipCode: $scope.zipCode});
     };
+    
+    $scope.disableTopicSearch = function() {
+      return _.isEmpty($scope.searchTopic);
+    };
+    
+    $scope.isNewCharNumber = function() {
+      var last = parseInt(_.last($scope.zipCode), 10);
+      
+      if (!(_.isNumber(last) && !_.isNaN(last))) {
+        $scope.zipCode = $scope.zipCode.slice(0, -1);
+      }
+    };
+    
+    $scope.disableZipSearch = function() {
+      return $scope.zipCode.length != 5;
+    };
   });
