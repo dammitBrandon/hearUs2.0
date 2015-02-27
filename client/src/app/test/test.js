8728aafe8f3f4e16e8e1d5879f9c1814490b4522
btbailey <brandon.bailey@slalom.com>
2015-02-26 22:35:57 -0600

    added extra state and template for additional testing

--- client/src/app/test/test.js
+++ client/src/app/test/test.js
@@ -6,20 +6,37 @@ angular.module('test', [
     $stateProvider
       .state('test', {
         url: '/test',
+        abstract: true,
         views: {
           "main": {
             controller: 'testingCtrl',
-            templateUrl: 'test/test-template.html'
-
+            templateUrl: 'templates/child-view-container.html'
           }
         },
-        data: {pageTitle: "test"},
+        data: {pageTitle: "test"}
+      })
+      .state('test.test1', {
+        url: '/test1',
+        views: {
+          "childView": {
+            templateUrl: 'test/test-template.html'
+          }
+        },
         access_level: ACCESS_LEVELS.authenticated
+      })
+      .state('test.test2', {
+        url: '/test2',
+        views: {
+          "childView": {
+            templateUrl: 'test/test-template2.html'
+          },
+          access_level: ACCESS_LEVELS.authenticated
+        }
       });
   })
   .controller('testingCtrl', function testingController($scope, $state, $log) {
     function init() {
-      $log.log('inside init');
+      $log.log('inside init testCtrl');
     }
 
     init();
