24b2f66b2acb8763f6a487d869aca8a9e6f051ce
btbailey <brandon.bailey@slalom.com>
2015-03-12 13:01:47 -0500

    Issue #11, user dashboard and controller updated to get additional data from profile service and display

--- client/src/app/dashboard/dashboard-controller.js
+++ client/src/app/dashboard/dashboard-controller.js
@@ -1,10 +1,23 @@
 angular.module('dashboard.controllers', [
-  'ui.router'
+  'ui.router',
+  'services.ProfileApi'
 ])
-  .controller('dashboardCtrl', function dashboardController($scope, $state, $log) {
+  .controller('dashboardCtrl', function dashboardController($scope, $state, profileService, $log) {
     function initDashboardCtrl() {
       $log.log('init dashboard');
+      $scope.senators = [];
+      $scope.user = profileService.getUser();
+      $log.log('user profile', $scope.user);
+      if (!_.isUndefined($scope.user.congressmen)) {
+        _.forEach($scope.user.congressmen, function (congressman) {
+          if (congressman.chamber === 'senate') {
+            $scope.senators.push(congressman);
+          } else if (congressman.chamber === 'house') {
+            $scope.houseRep = congressman;
+          }
+        });
+      }
     }
-    
+
     initDashboardCtrl();
   });
